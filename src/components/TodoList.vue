307158ae1139ca0bd42d63d23d1b7520d1ab9ce3
hook94128 <53824487+hook94128@users.noreply.github.com>
2020-07-31 23:27:59 +0900

    modal 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,14 @@
 <template>
     <section>
-        <ul>
-            <li v-for="(todoItem, index) in propsdata" class="shadow">
+        <transition-group name="list" tag="ul">
+            <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
                 <i class="checkBtn fa fa-check" aria-hidden="true"></i>
                 {{ todoItem }}
                 <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
                     <i class="far fa-trash-alt" aria-hidden="true"></i>
                 </span>
             </li>
-        </ul>
+        </transition-group>
     </section>
 </template>
 
@@ -24,6 +24,20 @@ export default {
 </script>
 
 <style>
+    .list-item {
+        display: inline-block;
+        margin-right: 10px;
+    }
+    .list-move {
+        transition: transform 1s;
+    }
+    .list-enter-active, .list-leave-active {
+        transition: all 1s;
+    }
+    .list-enter, list-leave-to {
+        opacity: 0;
+        transform: translateY(30px);
+    }
     ul{
         list-style-type: none;
         padding-left: 0;
